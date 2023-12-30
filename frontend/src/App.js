e7374db6941204b8f44b5daae593c3dc233b0c62
chanukaaaa <it21281082@my.sliit.lk>
2023-12-30 18:50:54 +0530

    Pages setup completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home';
+import { ShopCategory } from './Pages/ShopCategory';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
 
 function App() {
   return (
@@ -8,10 +13,16 @@ function App() {
       <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route></Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/mens' element={<ShopCategory category = "men"/>}/>
+        <Route path='/womans' element={<ShopCategory category = "women"/>}/>
+        <Route path='/kids' element={<ShopCategory category = "kid"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>} />
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
-
-
       </BrowserRouter>
     </div>
   );
