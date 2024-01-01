32140303d861928ffda681667ae922133d461c4f
chanukaaaa <it21281082@my.sliit.lk>
2024-01-01 20:07:56 +0530

    Hero completed

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png';
-import hero_image from '../Assets/hero_image.png';
+import hero_image2 from '../Assets/hero_image2.png';
 
 export const Hero = () => {
   return (
@@ -23,7 +23,7 @@ export const Hero = () => {
             </div>
         </div>
         <div className='hero-right'>
-            <img src={hero_image} alt="" />
+            <img src={hero_image2} alt="" />
         </div>
     </div>
   )
