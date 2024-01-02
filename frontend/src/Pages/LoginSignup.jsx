047a8062bdda8f8b976981a8a567c2160fb28e8b
chanukaaaa <it21281082@my.sliit.lk>
2024-01-02 19:40:23 +0530

    Login Signup Completed

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an Account? <span>Login here...</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox"  name='' id='' />
+          <p>I agree to the terms and conditions.</p>
+        </div>
+      </div>
+    </div>
   )
 }
